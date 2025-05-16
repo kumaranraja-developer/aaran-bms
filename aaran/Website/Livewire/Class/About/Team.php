39da165e7cb2809a84c01071001e78a62a196083
SUNDAR <aaransoftware@gmail.com>
2025-05-16 22:48:32 +0530

    #26 refactor teams

--- aaran/Website/Livewire/Class/About/Team.php
+++ aaran/Website/Livewire/Class/About/Team.php
@@ -2,13 +2,127 @@
 
 namespace Aaran\Website\Livewire\Class\About;
 
-use Livewire\Attributes\Layout;
+use Aaran\Assets\Traits\ComponentStateTrait;
+use Aaran\Website\Models\DevTeam;
+use Illuminate\Support\Str;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class Team extends Component
 {
+    use ComponentStateTrait;
+
+    #[Validate]
+    public string $vname = '';
+    public string $role = '';
+    public mixed $photo;
+    public mixed $about = '';
+    public string $mail = '';
+    public string $mobile = '';
+    public string $fb = '';
+    public string $twitter = '';
+    public string $msg = '';
+    public bool $active_id = true;
+
+    public function rules(): array
+    {
+        return [
+            'vname' => 'required',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'vname.required' => ':attribute is missing.',
+        ];
+    }
+
+    public function validationAttributes(): array
+    {
+        return [
+            'vname' => 'Team Member name',
+        ];
+    }
+
+    public function getSave(): void
+    {
+        $this->validate();
+
+        DevTeam::updateOrCreate(
+            ['id' => $this->vid],
+            [
+                'vname' => Str::ucfirst($this->vname),
+                'role' => $this->role,
+                'photo' => $this->photo,
+                'about' => $this->about,
+                'mail' => $this->mail,
+                'mobile' => $this->mobile,
+                'fb' => $this->fb,
+                'twitter' => $this->twitter,
+                'msg' => $this->msg,
+                'active_id' => $this->active_id
+            ],
+        );
+
+        $this->dispatch('notify', ...['type' => 'success', 'content' => ($this->vid ? 'Updated' : 'Saved') . ' Successfully']);
+        $this->clearFields();
+    }
+    public function clearFields(): void
+    {
+        $this->vid = null;
+        $this->vname = '';
+        $this->role = '';
+        $this->photo = '';
+        $this->about = '';
+        $this->mail = '';
+        $this->mobile = '';
+        $this->fb = '';
+        $this->twitter = '';
+        $this->msg = '';
+        $this->active_id = true;
+        $this->searches = '';
+    }
+
+    public function getObj(int $id): void
+    {
+        if ($obj = DevTeam::find($id)) {
+            $this->vid = $obj->id;
+            $this->vname = $obj->vname;
+            $this->role = $obj->role;
+            $this->photo = $obj->photo;
+            $this->about = $obj->about;
+            $this->mail = $obj->mail;
+            $this->mobile = $obj->mobile;
+            $this->fb = $obj->fb;
+            $this->twitter = $obj->twitter;
+            $this->msg = $obj->msg;
+            $this->active_id = $obj->active_id;
+        }
+    }
+
+    public function getList()
+    {
+        return DevTeam::active($this->activeRecord)
+            ->when($this->searches, fn($query) => $query->searchByName($this->searches))
+            ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')
+            ->paginate($this->perPage);
+    }
+
+    public function deleteFunction(): void
+    {
+        if (!$this->deleteId) return;
+
+        $obj = DevTeam::find($this->deleteId);
+        if ($obj) {
+            $obj->delete();
+        }
+    }
+
     public function render()
     {
-        return view('website::about.team');
+        return view('website::about.team', [
+            'list' => $this->getList()
+        ]);
     }
 }
--- aaran/Website/Livewire/Views/about/team.blade.php
+++ aaran/Website/Livewire/Views/about/team.blade.php
@@ -1,3 +1,91 @@
 <div>
+    <x-slot name="header">Team List</x-slot>
 
+    <x-Ui::forms.m-panel>
+
+
+        <!-- Top Controls --------------------------------------------------------------------------------------------->
+        <x-Ui::forms.top-controls :show-filters="$showFilters"/>
+
+        <!-- Table Caption -------------------------------------------------------------------------------------------->
+        <x-Ui::table.caption :caption="'Team'">
+            {{$list->count()}}
+        </x-Ui::table.caption>
+
+        <!-- Table Data ----------------------------------------------------------------------------------------------->
+
+        <x-Ui::table.form>
+            <x-slot:table_header>
+                <x-Ui::table.header-serial/>
+                <x-Ui::table.header-text wire:click.prevent="sortBy('id')" sortIcon="{{$sortAsc}}" :left="true">
+                    Name
+                </x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">Role</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">Photo</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">About</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">Mail</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">Mobile</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">FB</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">Twitter</x-Ui::table.header-text>
+                <x-Ui::table.header-text sortIcon="none">Msg</x-Ui::table.header-text>
+                <x-Ui::table.header-status/>
+                <x-Ui::table.header-action/>
+            </x-slot:table_header>
+
+            <x-slot:table_body>
+                @foreach($list as $index=>$row)
+                    <x-Ui::table.row>
+                        <x-Ui::table.cell-text>{{$index+1}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->vname}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->role}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->photo}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->about}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->mail}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->mobile}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->fb}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->twitter}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text left>{{$row->msg}}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-status active="{{$row->active_id}}"/>
+                        <x-Ui::table.cell-action id="{{$row->id}}"/>
+                    </x-Ui::table.row>
+                @endforeach
+            </x-slot:table_body>
+        </x-Ui::table.form>
+
+        <!-- Delete Modal --------------------------------------------------------------------------------------------->
+        <x-Ui::modal.confirm-delete/>
+
+        <div class="pt-5">{{ $list->links() }}</div>
+
+        <!-- Create/ Edit Popup --------------------------------------------------------------------------------------->
+
+        <x-Ui::forms.create :id="$vid">
+            <div class="flex flex-col gap-3">
+
+                <div>
+                    <x-Ui::input.floating wire:model="vname" label="Name"/>
+                    <x-Ui::input.error-text wire:model="vname"/>
+                </div>
+
+                <x-Ui::input.floating wire:model="role" label="Role"/>
+
+                <x-Ui::input.floating wire:model="photo" label="Photo"/>
+
+                <x-Ui::input.floating wire:model="about" label="About"/>
+
+                <x-Ui::input.floating wire:model="mail" label="Mail"/>
+
+                <x-Ui::input.floating wire:model="mobile" label="Mobile"/>
+
+                <x-Ui::input.floating wire:model="fb" label="Fb"/>
+
+                <x-Ui::input.floating wire:model="twitter" label="Twitter"/>
+
+                <x-Ui::input.floating wire:model="msg" label="Msg"/>
+
+            </div>
+
+        </x-Ui::forms.create>
+
+    </x-Ui::forms.m-panel>
 </div>
