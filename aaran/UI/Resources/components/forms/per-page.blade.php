81acbe07ea1c3473adc29c6ab6d76979df968d2a
SUNDAR <aaransoftware@gmail.com>
2025-05-17 11:56:40 +0530

    #32 refactor per page

--- aaran/UI/Resources/components/forms/per-page.blade.php
+++ aaran/UI/Resources/components/forms/per-page.blade.php
@@ -1,14 +1,10 @@
-<x-Ui::input.group borderless paddingless for="perPage" label="">
-    <div class="flex items-center space-x-2 sm:space-x-3">
-        <label for="perPage" class="text-sm font-medium text-gray-700">Per Page</label>
-        <select wire:model.live="perPage" id="perPage"
-                class="w-20 text-center rounded-md border border-gray-300 bg-white text-sm text-gray-700 shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none">
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-        </select>
-    </div>
-</x-Ui::input.group>
-
-
+<div class="flex items-center justify-between gap-3 p-2">
+    <label for="perPage" class="text-gray-400">Per Page</label>
+    <select wire:model.live="perPage" id="perPage"
+            class="w-20 border  rounded-md h-10 p-2 border-gray-200 focus:border-0 focus:ring-2 focus:ring-blue-500">
+        <option value="10">10</option>
+        <option value="25">25</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+    </select>
+</div>
