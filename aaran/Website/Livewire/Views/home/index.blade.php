e0e41aef7490227128d62de8efc34db27c63cbb8
saranR <720721106096@hicet.ac.in>
2025-05-18 22:10:08 +0530

    #47 working on home page for showing service-card and using the svg file as a blad component

--- aaran/Website/Livewire/Views/home/index.blade.php
+++ aaran/Website/Livewire/Views/home/index.blade.php
@@ -2,6 +2,7 @@
     <x-Ui::slider.new-1/>
     <x-Ui::web.home.hero/>
 {{--    <x-Ui::web.home.testimony/>--}}
+    <x-Ui::web.home.service-section/>
     <x-Ui::web.project.price/>
     <x-Ui::web.home.launch/>
     <x-Ui::web.common.footer-address/>
