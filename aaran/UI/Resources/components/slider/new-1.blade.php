0e559dd039cc9d5a5a04ab851c5bbe657b2cf378
SUNDAR <aaransoftware@gmail.com>
2025-05-21 13:31:11 +0530

    #75 fixing on home

--- aaran/UI/Resources/components/slider/new-1.blade.php
+++ aaran/UI/Resources/components/slider/new-1.blade.php
@@ -130,17 +130,20 @@
                         @endforeach
 
                         <div class="mt-10 flex justify-center gap-x-6" data-animatable>
+
                             <a class="group inline-flex items-center justify-center rounded-full py-2 px-4 text-sm font-semibold focus:outline-hidden focus-visible:outline-2 focus-visible:outline-offset-2 bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900"
                                href="{{ route('client-registration') }}">Get {{Aaran\Assets\Config\Application::AppTrialPeriod}} free</a>
-                            <a class="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-hidden ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300"
-                               href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-                                <svg aria-hidden="true"
-                                     class="h-3 w-3 flex-none fill-blue-600 group-active:fill-current">
-                                    <path
-                                        d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z"></path>
-                                </svg>
-                                <span class="ml-3">Watch video</span>
-                            </a>
+
+{{--                            <a class="group inline-flex ring-1 items-center justify-center rounded-full py-2 px-4 text-sm focus:outline-hidden ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300"--}}
+{{--                               href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">--}}
+{{--                                <svg aria-hidden="true"--}}
+{{--                                     class="h-3 w-3 flex-none fill-blue-600 group-active:fill-current">--}}
+{{--                                    <path--}}
+{{--                                        d="m9.997 6.91-7.583 3.447A1 1 0 0 1 1 9.447V2.553a1 1 0 0 1 1.414-.91L9.997 5.09c.782.355.782 1.465 0 1.82Z"></path>--}}
+{{--                                </svg>--}}
+{{--                                <span class="ml-3">Watch video</span>--}}
+{{--                            </a>--}}
+
                         </div>
                     </div>
                 </div>
