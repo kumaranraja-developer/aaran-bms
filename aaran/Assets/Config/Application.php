74d3ab3a837dc2e0309349f9d7f5ec1b32b8d7dc
SUNDAR <aaransoftware@gmail.com>
2025-05-21 11:35:40 +0530

    #70 fixing address

--- aaran/Assets/Config/Application.php
+++ aaran/Assets/Config/Application.php
@@ -7,10 +7,10 @@ class Application
     public const AppName = 'Codexsun';
     public const AppVersion = '1.0.0';
     public const AppCompanyName = 'Aaran Software';
-    public const AppCompanyAddress_1 = 'No.7, Anjal Nagar';
-    public const AppCompanyAddress_2 = '3rd Street';
-    public const AppCompanyAddress_3 = 'Postal colony';
-    public const AppCompanyAddress_4 = 'Tiruppur - 641602';
+    public const AppCompanyAddress_1 = 'No.36, M.C.B.Blossom Rich Street';
+    public const AppCompanyAddress_2 = 'Mettupalayam';
+    public const AppCompanyAddress_3 = 'Avadi';
+    public const AppCompanyAddress_4 = 'Chennai - 600077';
     public const AppCompanyAddress_5 = 'Tamilnadu, India';
     public const AppCompanyEmail = 'info@aaransofware.com';
     public const AppCompanyMobile = '+91 9655227767';
