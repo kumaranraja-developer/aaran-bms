714de3091085186db3f29aeb25e68bf1aeff3597
saranR <720721106096@hicet.ac.in>
2025-05-21 23:47:49 +0530

    #84 complete the testimonial model

--- aaran/Website/Livewire/Views/about/testimonial-list.blade.php
+++ aaran/Website/Livewire/Views/about/testimonial-list.blade.php
@@ -26,6 +26,9 @@
                 <x-Ui::table.header-text    sortIcon="none" :center="true">
                     Photo
                 </x-Ui::table.header-text>
+                <x-Ui::table.header-text  sortIcon="none" :center="true">
+                    Address
+                </x-Ui::table.header-text>
                 <x-Ui::table.header-text    sortIcon="none" :center="true">
                     Testimonial
                 </x-Ui::table.header-text>
@@ -45,8 +48,10 @@
                                 <img src="{{ asset('storage/photos/' . $row->photo) }}" class="h-10 w-10 rounded-full object-cover" alt="photo" />
                             @else
                                 N/A
-                            @endif                        </x-Ui::table.cell-text>
-                        <x-Ui::table.cell-text left>{{$row->testimonial}}</x-Ui::table.cell-text>
+                            @endif
+                        </x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text class="line-clamp-2" left>{!! $row->address !!}</x-Ui::table.cell-text>
+                        <x-Ui::table.cell-text  left>{{$row->testimonial}}</x-Ui::table.cell-text>
                         <x-Ui::table.cell-status active="{{$row->active_id}}"/>
                         <x-Ui::table.cell-action id="{{$row->id}}"/>
                     </x-Ui::table.row>
@@ -60,19 +65,69 @@
         <div class="pt-5">{{ $list->links() }}</div>
 
         <!-- Create/ Edit Popup --------------------------------------------------------------------------------------->
-
         <x-Ui::forms.create :id="$vid">
-            <div class="flex flex-col gap-3 w-full">
 
-            <x-Ui::input.floating wire:model="vname" label="Name" />
-            <x-Ui::input.error-text wire:model="vname"/>
-            <x-Ui::input.floating wire:model="company" label="Company" />
-            <x-Ui::input.floating type="file" wire:model="photo" label="Photo" />
-            <x-Ui::input.floating wire:model="testimonial" label="Testimonial" />
+            <div class="flex flex-col gap-4 w-full">
+
+                <!-- Name + Photo Upload -->
+                <div class="flex flex-row gap-6 items-start">
+
+                    <!-- Name Input -->
+                    <div class="flex flex-col gap-6 flex-1">
+
+                        <div>
+                        <x-Ui::input.floating wire:model="vname" label="Name" />
+                        <x-Ui::input.error-text wire:model="vname"/>
+                        </div>
+                        <div>
+                        <x-Ui::input.floating wire:model="company" label="Company" />
+                        </div>
+
+                    </div>
+
+                    <!-- Image Upload -->
+                    <div class="flex flex-col">
+                        <label for="bg_image" class="text-zinc-500 text-sm font-medium mb-1">Image</label>
+
+                        <div class="flex flex-row gap-3 items-center">
+                            @if($photo)
+                                <div class="bg-blue-100 p-1 rounded-lg overflow-hidden">
+                                    <img
+                                        class="w-[156px] h-[89px] rounded-lg hover:brightness-110 hover:scale-105 duration-300 transition-all ease-out"
+                                        src="{{ $photo }}"
+                                        alt="{{ $photo ?: '' }}"/>
+                                </div>
+                            @endif
+
+                            <label for="bg_image"
+                                   class="text-gray-500 font-medium text-sm rounded flex flex-col items-center
+                                  justify-center cursor-pointer border-2 border-gray-300 border-dashed p-2
+                                  w-[160px] h-[100px] hover:border-gray-400 transition">
+                                <x-Ui::icons.icon icon="cloud-upload" class="w-8 h-auto text-gray-400 mb-1"/>
+                                <span class="text-sm">Upload Photo</span>
+                                <input type="file" id="bg_image" wire:model="photo" class="hidden"/>
+                                <p class="text-xs text-gray-400 mt-1 text-center">PNG or JPG</p>
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <!-- Company -->
+
+
+                <!-- Address -->
+                <x-Ui::input.floating-textarea wire:model="address" label="Address"/>
+
+                <!-- Testimonial -->
+                <x-Ui::input.floating-textarea wire:model="testimonial" label="Testimonial" />
 
             </div>
 
+
+
+
+
         </x-Ui::forms.create>
+
 
     </x-Ui::forms.m-panel>
 </div>
