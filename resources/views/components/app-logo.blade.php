f5342df5e583286afd24c63cfb549e146758cf7e
SUNDAR <aaransoftware@gmail.com>
2025-06-04 08:56:24 +0530

    #115 refactor on login

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,7 @@
 <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-Ui::logo.cxlogo  icon="light" class="size-5 fill-current text-white dark:text-black"/>
+{{--    <x-Ui::logo.cxlogo  icon="light" class="size-5 fill-current text-white dark:text-black"/>--}}
+
+{{--    <x-Ui::logo.cxlogo  icon="orange"/>--}}
 {{--    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />--}}
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,7 +1,7 @@
-    <x-auth-session-status class="text-center" :status="session('status')" />
+{{--    <x-auth-session-status class="text-center" :status="session('status')" />--}}
 
-<div class="flex flex-col gap-6 rounded-2xl border shadow-2xl shadow-blue-500 p-10">
-{{--    <x-auth-header :title="__('')" :description="__('')" />--}}
+<div class="flex flex-col gap-6 rounded-2xl border shadow-2xl shadow-blue-500/50 p-10">
+    {{--    <x-auth-header :title="__('')" :description="__('')" />--}}
     <!-- Session Status -->
 
     <form wire:submit="login" class="flex flex-col gap-6">
@@ -28,25 +28,25 @@
                 viewable
             />
 
-{{--            @if (Route::has('password.request'))--}}
-{{--                <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>--}}
-{{--                    {{ __('Forgot your password?') }}--}}
-{{--                </flux:link>--}}
-{{--            @endif--}}
+            {{--            @if (Route::has('password.request'))--}}
+            {{--                <flux:link class="absolute end-0 top-0 text-sm" :href="route('password.request')" wire:navigate>--}}
+            {{--                    {{ __('Forgot your password?') }}--}}
+            {{--                </flux:link>--}}
+            {{--            @endif--}}
         </div>
 
         <!-- Remember Me -->
-        <flux:checkbox wire:model="remember" :label="__('Remember me')" />
+        {{--        <flux:checkbox wire:model="remember" :label="__('Remember me')" />--}}
 
         <div class="flex items-center justify-end">
             <flux:button variant="primary" type="submit" class="w-full">{{ __('Log in') }}</flux:button>
         </div>
     </form>
 
-    @if (Route::has('register'))
-        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            {{ __('Don\'t have an account?') }}
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
-        </div>
-    @endif
+    {{--    @if (Route::has('register'))--}}
+    {{--        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">--}}
+    {{--            {{ __('Don\'t have an account?') }}--}}
+    {{--            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>--}}
+    {{--        </div>--}}
+    {{--    @endif--}}
 </div>
