ea606651c83289445dee50e42f6e1de700049833
kumaranraja <kumaranraja46248@gmail.com>
2025-07-17 10:15:52 +0530

    145 featured apps

--- aaran/UI/Resources/components/web/project/featured-apps.blade.php
+++ aaran/UI/Resources/components/web/project/featured-apps.blade.php
@@ -8,16 +8,16 @@
     'btn_bg'=>"bg-green-600"
 ])
 
-<div class="bg-white dark:bg-dark-4  border rounded-lg border-neutral-300 hover:-translate-y-1.5 transform duration-500 h-full p-2 max-w-md w-full mx-auto">
+<div class="bg-bg-1 dark:bg-bg-3  border rounded-lg border-neutral-300 hover:-translate-y-1.5 transform duration-500 h-full p-2 max-w-md w-full mx-auto">
     <a  href="{{route('bill-books')}}" role="button"
-         class="{{$bg}} rounded-xs border dark:bg-dark-5 dark:text-dark-9 border-neutral-200 p-4 h-full flex flex-col justify-between">
+         class="{{$bg}} rounded-xs border dark:bg-bg-3 dark:text-text-1 border-neutral-200 p-4 h-full flex flex-col justify-between">
         <div>
             <img src="{{ asset($icon) }}" alt="" class="w-24 h-24 object-contain" />
         </div>
         <div class="flex flex-col py-2 flex-grow">
-            <div class="text-xl font-semibold dark:text-dark-9">{{ $title }}</div>
-            <div class="my-3 text-neutral-500 flex-grow dark:text-dark-8">{{ $description }}</div>
-            <button class="text-md px-3 w-max text-white py-2  cursor-pointer {{$btn}} {{$btn_bg}} my-4 mt-auto text-start">{{$btn_text}}</button>
+            <div class="text-xl font-semibold dark:text-text-1">{{ $title }}</div>
+            <div class="my-3 text-text-4 flex-grow dark:text-text-2">{{ $description }}</div>
+            <button class="text-md px-3 w-max text-text-1 py-2  cursor-pointer {{$btn}} {{$btn_bg}} my-4 mt-auto text-start">{{$btn_text}}</button>
         </div>
     </a>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
-
     Route::get('settings/profile', Profile::class)->name('settings.profile');
     Route::get('settings/password', Password::class)->name('settings.password');
     Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
